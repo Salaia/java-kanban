1c3de2d329e56ad55b48de065e46a50389b290e7
Salaia <puma.hope@yandex.ru>
2023-12-07 14:31:04 +0300

    main clean-up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,9 @@ import http.HttpTaskServer;
 import http.KVServer;
 import managers.Managers;
 import managers.TaskManager;
-import tasks.Task;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.Month;
 
 public class Main {
 
@@ -20,48 +13,13 @@ public class Main {
     static HttpClient client;
     static Gson gson;
 
-/*
-    Тут лежит предварительное тестирование из ТЗ, то есть код с платформы, пусть лежит как изначальный образец
- */
-
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         new KVServer().start();
         new HttpTaskServer().start();
 
         taskManager = Managers.getDefault();
-        //taskManager = new HttpTaskManager(8080);
         client = HttpClient.newHttpClient();
         gson = Managers.getGson();
-
-        // долго думала, куда запихнуть подсказки из ТЗ, место нашла только в предварительном тестировании
-        /*createSimpleTaskTest();
-        getSimpleTaskByIdTest();
-        getAllTasksTest();*/
-
-    }
-
-    public static void getAllTasksTest() throws IOException, InterruptedException {
-        // Подсказка: как получить все задачи
-        URI url = URI.create("http://localhost:8080/tasks/task");
-        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public static void createSimpleTaskTest() throws IOException, InterruptedException {
-        // Подсказка: как создать задачу
-        URI url = URI.create("http://localhost:8080/tasks/task");
-        Task newTask = new Task("Second SimpleTask", "SimpleTask(ID=2) with DateTime", LocalDateTime.of(2023, Month.MARCH, 5, 14, 0, 0), Duration.ofMinutes(15));
-        String json = gson.toJson(newTask);
-        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
-        HttpRequest request = HttpRequest.newBuilder().uri(url).POST(body).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public static void getSimpleTaskByIdTest() throws IOException, InterruptedException {
-        // Подсказка: как получить задачу с id = 1
-        URI url = URI.create("http://localhost:8080/tasks/task?id=1");
-        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
 }
